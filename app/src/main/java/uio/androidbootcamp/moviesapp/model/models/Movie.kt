d644862659a0d2ce7117267b8db5ba7cadbed014
Jorge Rodriguez Chala <jorgerodriguezchala@Jorges-MacBook-Pro.local>
2018-05-20 11:04:02 -0500

    Creacion de atributos y constructores de la clase Movie

--- app/src/main/java/uio/androidbootcamp/moviesapp/model/models/Movie.kt
+++ app/src/main/java/uio/androidbootcamp/moviesapp/model/models/Movie.kt
@@ -1,4 +1,9 @@
 package uio.androidbootcamp.moviesapp.model.models
 
-class Movie {
+import java.util.*
+
+class Movie (val id: Long, val posterPath: String, val name: String, val overview: String, val releaseDate: Date) {
+
+    constructor() : this(0,"","","", Date())
+
 }
